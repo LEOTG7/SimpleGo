de23a2d4e6ad00b99ecb8b8bc245403dc84bab02
ʟᴇᴏ ᠰ ᴛɢ <ilashadam039@gmail.com>
2025-05-31 16:48:51 +0400

    Update main.go

--- main.go
+++ main.go
@@ -9,3 +9,21 @@ import (
        "github.com/LEOTG7/SimpleGo/plugins"
 
 )
+
+func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			// Print reason for panic + stack for some sort of helpful log output
+			fmt.Println(r)
+			fmt.Println(string(debug.Stack()))
+		}
+	}()
+
+	
+       go func() {
+		http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
+			fmt.Fprintf(w, "Hi Hi")
+		})
+
+		http.ListenAndServe(":"+config.Port, nil)
+	}()
