1b9a828914619f2882df71bed5d26f277064d50c
ʟᴇᴏ ᠰ ᴛɢ <ilashadam039@gmail.com>
2025-06-01 11:21:23 +0400

    Update dispatcher.go

--- plugins/dispatcher.go
+++ plugins/dispatcher.go
@@ -1,1 +1,24 @@
 
+package plugins
+
+import (
+	"fmt"
+
+	"github.com/PaulSonOfLars/gotgbot/v2"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext"
+	"github.com/PaulSonOfLars/gotgbot/v2/ext/handlers"
+)
+
+const basicCommandsGroup = 1
+
+var Dispatcher = ext.NewDispatcher(&ext.DispatcherOpts{
+	Error: func(b *gotgbot.Bot, ctx *ext.Context, err error) ext.DispatcherAction {
+		fmt.Println("An error occurred while handling update:", err.Error())
+		return ext.DispatcherActionNoop
+	},
+})
+
+
+func init() {
+	Dispatcher.AddHandlerToGroup(handlers.NewCommand("start", Start), basicCommandsGroup)
+}
