a2ca06d5901e478a707bd0239ea74179fa4e482c
ʟᴇᴏ ᠰ ᴛɢ <ilashadam039@gmail.com>
2025-06-11 00:40:58 +0400

    Update Start.go

--- plugins/Start.go
+++ plugins/Start.go
@@ -7,7 +7,28 @@ import (
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
 )
 
-func Start(b *gotgbot.Bot, ctx *ext.Context) error {
-	_, err := ctx.EffectiveMessage.Reply(b, fmt.Sprintf("Hello, @%s! I'm alive ✅", ctx.EffectiveUser.Username), nil)
+func Start(bot *gotgbot.Bot, ctx *gotgbot.Context) error {
+	_, err := bot.SendMessage(
+		ctx.EffectiveChat.Id,
+		fmt.Sprintf("Hello, @%s! I'm alive ✅", ctx.EffectiveUser.Username),
+		&gotgbot.SendMessageOpts{
+			ParseMode: gotgbot.ParseModeHTML,
+			ReplyMarkup: &gotgbot.InlineKeyboardMarkup{
+				InlineKeyboard: [][]gotgbot.InlineKeyboardButton{
+					{
+						{
+							Text:         "Click Me!",
+							CallbackData: "clicked",
+						},
+					},
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		fmt.Println("Failed to send start message:", err)
+	}
+
 	return err
 }
