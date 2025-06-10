0274e7ca1b2afaba44ddfa0549b5757c0d9e544b
ʟᴇᴏ ᠰ ᴛɢ <ilashadam039@gmail.com>
2025-06-11 00:43:57 +0400

    Create callback.go

---
+++ plugins/callback.go
@@ -0,0 +1,26 @@
+import (
+	"fmt"
+
+	"github.com/PaulSonOfLars/gotgbot/v2"
+)
+
+func HandleEditTextCallback(bot *gotgbot.Bot, ctx *gotgbot.Context) error {
+	_, err := bot.EditMessageText(
+		ctx.CallbackQuery.Message.Chat.Id,
+		ctx.CallbackQuery.Message.MessageId,
+		"😂😂 Nothing Here",
+		&gotgbot.EditMessageTextOpts{
+			ParseMode: gotgbot.ParseModeHTML,
+		},
+	)
+	if err != nil {
+		fmt.Println("Failed to edit message text:", err)
+		return err
+	}
+
+	_, err = ctx.CallbackQuery.Answer(bot, nil)
+	if err != nil {
+		fmt.Println("Failed to answer callback query:", err)
+	}
+	return err
+}
